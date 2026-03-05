22c79a395eac4916bf4bafb291001ba15ee94d60
krishnalilhare801-ship-it <krishnalilhare801@gmail.com>
2026-03-05 13:09:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,15 +13,15 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
+API_ID = int(environ.get('API_ID', '30618142'))
+API_HASH = environ.get('API_HASH', '17dc8c3b7410f89f5028eda7fa0487d4')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8781004762:AAH0NHZaosPsvDHFl7P1_Qdo8mzAxRbMTaE')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6934918664').split()]
+USERNAME = environ.get('USERNAME', "t.me/MN_movie_bot") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002118498742'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', '@newallmoviesearchgroup')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
